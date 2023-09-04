093d70de742bb891d30f657a7145098409e34b7d
muhammad rifqi <muhammad45rifki@gmail.com>
2023-09-04 16:04:08 +0700

    qqq

---
+++ cart.php
@@ -0,0 +1,52 @@
+<?php
+
+        session_start();
+
+        header("Access-Control-Allow-Origin: *");
+		header("Access-Control-Allow-Credentials: true");
+		header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+		header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+        
+        include "config.php";
+
+		$headers = getallheaders();
+        if (!isset($headers['Authorization'])) {
+          http_response_code(401);
+          exit();
+        }else{
+            
+            $tokens = explode(' ', $headers['Authorization']);
+            $sql = mysqli_query($koneksi,"select * from pengguna where token = '".$tokens[1]."'");
+            $check = mysqli_num_rows($sql);
+            
+            if($check > 0){
+                
+                $data = json_decode(file_get_contents("php://input"),true);
+                $sesionid = session_id();
+                $cleartag = str_replace("'"," ", $data['keterangan']);
+                $sql = mysqli_query($koneksi,"insert into cart(kode_barang,nama_barang,stok,harga,photo, keterangan, id_kategori, id_koperasi, session_id)values('".$data['kode_barang']."','".$data['nama_barang']."','".$data['stok']."','".$data['harga']."','".$data['photo']."','".$cleartag."','".$data['id_kategori']."','".$data['id_koperasi']."','".$sesionid."')");
+                $ids = mysqli_insert_id($koneksi);
+                $sql = mysqli_query($koneksi,"select * from cart where session_id = '".$sesionid."'");
+                if($sql){
+                    $rr = mysqli_fetch_assoc($sql);
+                    echo json_encode(
+                        array(
+                            'response_code' => 200,
+                            'message' => 'Success',
+                            'data' => $rr
+                        )
+                    );
+                }
+        	
+            }else{
+                echo json_encode(
+                    array(
+                        'response_code' => 401,
+                        'message' => 'Gagal Mengambil Data!'
+                    )
+                    );
+                }
+        }        
+            
+
+?>
