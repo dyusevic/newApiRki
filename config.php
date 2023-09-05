795facd008187fe547ff5d84d5fb5e2b613c2a8c
Muhammad Rifqi <ats.muhammadr@xl.co.id>
2023-09-05 20:02:40 +0700

    aaaa

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
- //$koneksi = mysqli_connect("localhost","root","RvVeByyr>8ZB","api_koperasi");
- $koneksi = mysqli_connect("localhost","root","","api_koperasi");
+ $koneksi = mysqli_connect("localhost","root","RvVeByyr>8ZB","api_koperasi");
+ //$koneksi = mysqli_connect("localhost","root","","api_koperasi");
 ?>
