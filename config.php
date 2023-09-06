28d04ca6f4e1d788478c59783df4de1c6deb7e11
muhammad rifqi <muhammad45rifki@gmail.com>
2023-09-06 11:06:08 +0700

    aaaa

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
- //$koneksi = mysqli_connect("localhost","root","RvVeByyr>8ZB","api_koperasi");
- $koneksi = mysqli_connect("localhost","root","","api_koperasi");
+ $koneksi = mysqli_connect("localhost","root","RvVeByyr>8ZB","api_koperasi");
+ //$koneksi = mysqli_connect("localhost","root","","api_koperasi");
  $url_live = "http://34.28.160.3/newApiRki";
  $url_local = "http://localhost/newApiRki";
 
